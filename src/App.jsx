c9808d67e766b2e00cf549f7a6962096a4ce8b51
Omermymon <omermymon17@gmail.com>
2024-12-05 14:09:21 +0100

    editdata

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,29 @@ import "./styles/App.css";
 import { TextField, Button } from "@mui/material";
 
 function App() {
-  const [username, setUsername] = useState("user");
-  const [fullname, setFullname] = useState("full name");
-  const [age, setAge] = useState("30");
-  const [showResults, setShowResults] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [currentTodo, setCurrentTodo] = useState();
 
-  const handleAgeChange = (event) => {
-    setAge(event.target.value);
-  };
-  const handleNameChange = (event) => {
-    setUsername(event.target.value);
-  };
-  const handleFullNameChange = (event) => {
-    setFullname(event.target.value);
-  };
-
-  const handleClick = () => {
-    setShowResults(true);
+  const handleAdd = () => {
+    setTodos([...todos, currentTodo]); // Add the new todo to the array
+    setCurrentTodo(""); // Clear the input field
   };
 
   return (
     <div>
       <div>
-        <h1>Username</h1>
-        <TextField value={username} onChange={handleNameChange} />
-        <h1>Full name</h1>
-        <TextField value={fullname} onChange={handleFullNameChange} />
-        <h1>Age</h1>
-        <TextField value={age} onChange={handleAgeChange} />
+        <TextField
+          value={currentTodo}
+          onChange={(e) => setCurrentTodo(e.target.value)}
+          placeholder="Enter a todo"
+        />
+        <Button onClick={handleAdd}>Add Todo</Button>
       </div>
-      <Button onClick={handleClick}>Press here</Button>
-
-      {showResults && (
-        <div>
-          <h2>{`Username: ${username}`}</h2>
-          <h2>{`Full Name: ${fullname}`}</h2>
-          <h2>{`Age: ${age}`}</h2>
-        </div>
-      )}
+      <div>
+        {todos.map((todo, index) => (
+          <div key={index}>{todo}</div>
+        ))}
+      </div>
     </div>
   );
 }
