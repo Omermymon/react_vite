be60192d3a5e28dc4cc7f304ff03828f6145d143
Omermymon <omermymon17@gmail.com>
2024-12-29 16:09:00 +0100

    linux

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src", // Optional: Helps with cleaner imports like "@/components/MyComponent"
+      "@": "/src", 
     },
   },
   css: {
     preprocessorOptions: {
       css: {
-        charset: false, // Avoid issues with BOM in CSS files
+        charset: false, 
       },
     },
   },
   server: {
-    port: 3000, // Optional: Customize your local development port
+    port: 3000, 
   },
   build: {
-    outDir: "dist", // Output directory for build files
-    emptyOutDir: true, // Clear the output directory before building
+    outDir: "dist", 
+    emptyOutDir: true, 
   },
 });
